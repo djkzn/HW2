da1dec6cb8d30be0cb23de12e369fdda2d1b581e
djkzn <zotov2007@gmail.com>
2026-04-02 10:56:23 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string name;
-    std::cin >> name; // читаем имя пользователя
+    std::cin >> name; // читаем имя пользователя, через cin
     std::cout << "Hello world from @" << name << std::endl; // выводим приветствие
     return 0;
 }
