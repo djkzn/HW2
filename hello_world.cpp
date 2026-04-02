0988079601fc3c6f1d72c66953e5b1209d5a98ca
djkzn <zotov2007@gmail.com>
2026-04-02 10:54:28 +0300

    apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string name;
-    std::cin >> name; // читаем имя пользователя, через cin
-    std::cout << "Hello world from @" << name << std::endl; // выводим приветствие
-    return 0;
+int
+main()
+{
+  std::string name;
+  std::cin >> name; // читаем имя пользователя
+  std::cout << "Hello world from @" << name << std::endl; // выводим приветствие
+  return 0;
 }
